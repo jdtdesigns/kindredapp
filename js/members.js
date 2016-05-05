561874375c9ed2c09791e411271a66e10edcdfed
JD <jt7903@gmail.com>
2016-05-05 14:02:01 -0400

    added members

--- js/members.js
+++ js/members.js
@@ -2,8 +2,14 @@ var members = {
 	'Boris Chantel': {
 		gmt: -4
 	},
+	'Joseph Davidson': {
+		gmt: -7
+	},
 	'Ali Hamdon': {
 		gmt: 1
+	},
+	'Alain Mbuku': {
+		gmt: -7
 	},
 	'JD Tadlock': {
 		gmt: -4
