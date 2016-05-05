9be3922feb0bef6a3eb8a2d4e2254dfeb043d793
JD <jt7903@gmail.com>
2016-05-05 17:52:12 -0400

    added no results feature to search

--- js/app.js
+++ js/app.js
@@ -30,28 +30,34 @@ var app = (function() {
 	};
 
 	var getMember = function() {
-		var input = this.value.toLowerCase(),
-			names = document.getElementsByClassName('Times__member-name'),
-			matches = [];
+		var input 		= this.value.toLowerCase(),
+			names 		= document.getElementsByClassName('Times__member-name'),
+			matches 	= [],
+			noResult 	= '';
 
-			if (!String.prototype.includes) {
-				String.prototype.includes = function() {
-					'use strict';
-					return String.prototype.indexOf.apply(this, arguments) !== -1;
-				};
-			}
+		if (!String.prototype.includes) {
+			String.prototype.includes = function() {
+				'use strict';
+				return String.prototype.indexOf.apply(this, arguments) !== -1;
+			};
+		}
 			
 		for ( var i = 0; i < names.length; i++ ) {
 			if ( names[i].innerHTML.toLowerCase().includes(input) ) {
-				matches.push(names[i]);
+				matches.push(names[i]);	
 			}
-
 			names[i].parentElement.classList.add('hide');
 		}
 
-		matches.map(function(name) {
-			name.parentElement.classList.remove('hide');
-		});
+		if ( matches.length === 0 ) {
+			document.getElementById('no-result').classList.remove('hide');
+		} else {
+			document.getElementById('no-result').className = 'hide';
+			matches.map(function(name) {
+				name.parentElement.classList.remove('hide');
+			});
+		}
+		
 	};
 
 	return {
