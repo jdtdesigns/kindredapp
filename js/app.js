3deed5686d53eceb592aa44daff9ee31e4e708dc
JD <jt7903@gmail.com>
2016-05-05 13:11:45 -0400

    updated member block padding and time font size

--- js/app.js
+++ js/app.js
@@ -7,16 +7,21 @@ var app = (function() {
 		var gmt 		= new Date(),
 			offset 		= gmt.getTimezoneOffset() / 60,
 			userTime 	= '',
+			userHours	= '',
+			userMintues	= '',
 			page 		= document.getElementsByClassName('Times__members')[0],
 			html 		= '';
 			
 			gmt.setHours(gmt.getHours() + offset);
+			
 			for ( var member in members ) {
-				userTime = new Date(new Date().setHours(gmt.getHours() + members[member].gmt));
+				userTime 	= new Date(new Date().setHours(gmt.getHours() + members[member].gmt));
+				userHours 	= userTime.getHours() < 10 ? '0' + userTime.getHours() : userTime.getHours();
+				userMinutes = userTime.getMinutes() < 10 ? '0' + userTime.getMinutes() : userTime.getMinutes();
 
 				html += '<div class="Times__member row column-center">' +
 							'<span class="Times__member-name">' + member + '</span>' +
-							'<span class="Times__member-time">'+ userTime.getHours() + ':' + userTime.getMinutes() + '</span>' +
+							'<span class="Times__member-time">'+ userHours + ':' + userMinutes + '</span>' +
 						'</div>';
 			}
 
--- js/members.js
+++ js/members.js
@@ -1,11 +1,11 @@
 var members = {
 	'Boris Chantel': {
-		gmt: -5
+		gmt: -4
 	},
 	'Ali Hamdon': {
 		gmt: 1
 	},
 	'JD Tadlock': {
-		gmt: -5
+		gmt: -4
 	}
 }
