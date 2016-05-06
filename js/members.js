b5c95fbc59199218819066ea95ba501ad8e3a84c
JD <jt7903@gmail.com>
2016-05-06 08:30:29 -0400

    added more members

--- js/members.js
+++ js/members.js
@@ -11,14 +11,23 @@ var members = {
 	'Robert DeSautel': {
 		gmt: -4
 	},
+	'Dima': {
+		gmt: 3
+	},
 	'Ali Hamdon': {
 		gmt: 1
+	},
+	'Mart Lepanen': {
+		gmt: 3
 	},
 	'Alain Mbuku': {
 		gmt: -7
 	},
 	'Rnwebdk': {
 		gmt: 2
+	},
+	'Nick Sherbakov': {
+		gmt: 3
 	},
 	'JD Tadlock': {
 		gmt: -4
